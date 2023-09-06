e78071c9fd1770c9bb5a4792458049df962e4873
Santhu <purisanthosh2002@gmail.com>
2023-09-06 21:12:05 +0530

    Integration of upload opportunities is done

--- src/My Components/PatronPages/OpportunitesforArtist/UploadOpportunities.js
+++ src/My Components/PatronPages/OpportunitesforArtist/UploadOpportunities.js
@@ -43,12 +43,23 @@ export function UploadOpportunities() {
 
   const changeHandler  = (event)=>{
     const {name , value} = event.target;
-    setFormData((prev)=>(
-      {
-        ...prev ,
-        [name]:value
-      }
-    ))
+    if (name.startsWith('applicationPeriod.')) {
+      // Handle nested property updates
+      const nestedProp = name.split('.')[1];
+      setFormData((prev) => ({
+        ...prev,
+        applicationPeriod: {
+          ...prev.applicationPeriod,
+          [nestedProp]: value,
+        },
+      }));
+    } else {
+      // Handle updates for other properties
+      setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+      }));
+    }
   }
 
   const submitHandler = async(event)=>{
@@ -59,6 +70,34 @@ export function UploadOpportunities() {
       console.log('uplares' , response);
       if(response.success ==='success'){
         toast.success('successfully uploaded');
+        setFormData({
+          artNature:"",
+          location:"",
+          performanceDate:"",
+          performanceType:"",
+          languages:"",
+          mediaType :"",
+          position:"",
+          category:"",
+          description:"",
+          requiredArtists:"",
+          artistLevel:"",
+          // mediaType :"",
+          // openings:"",
+          performanceDuration:"",
+          // location:"",
+          budget:"",
+          applicationPeriod:{
+            start:"",
+            end:""
+          },
+          otherRequirements:"",
+          incentives:"",
+          expertise:"",
+          timeSlot :"",
+          theme:""
+      
+        })
       }
       else{
         toast.error(response.message);
@@ -84,7 +123,7 @@ export function UploadOpportunities() {
             <div className='ArtistOpportunities_Page_Infoform_Contentone'>
              
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Nature of Art</label>
+                <label>Nature of Art*</label>
                 <input value={formData.artNature} name='artNature' onChange={changeHandler} type='text' placeholder='Enter nature of art'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
@@ -92,15 +131,15 @@ export function UploadOpportunities() {
                 <input value={formData.location} name='location' onChange={changeHandler} type='text' placeholder='Enter location'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Name of Art</label>
-                <input  name='' onChange={changeHandler} type='text' placeholder='Enter art name'></input>
+                <label>Description*</label>
+                <input  name='description' onChange={changeHandler} type='text' placeholder='Enter art Description'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
                 <label>Date of Performance*</label>
                 <input value={formData.performanceDate} name='performanceDate' onChange={changeHandler} type='text' placeholder='Enter event date'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Type of Performance</label>
+                <label>Type of Performance*</label>
                 <input value={formData.performanceType} name='performanceType' onChange={changeHandler} type='text' placeholder='Enter performance type'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
@@ -108,7 +147,7 @@ export function UploadOpportunities() {
                 <input value={formData.languages} name='languages' onChange={changeHandler} type='text' placeholder='Enter languages'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Live / Record</label>
+                <label>Live / Record *</label>
                 <select onChange={changeHandler} name='mediaType'>
                   <option   selected hidden>Select</option>
                   <option value="Live">Live</option>
@@ -116,50 +155,58 @@ export function UploadOpportunities() {
                 </select>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Theme</label>
+                <label>Theme*</label>
                 <input onChange={changeHandler} value={formData.theme} name='theme' type='text' placeholder='Enter theme'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>No of Artist</label>
+                <label>Number of Artist*</label>
                 <input value={formData.requiredArtists} name='requiredArtists' onChange={changeHandler} type='text' placeholder='Enter number of artist'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Time of Performance</label>
-                <input onChange={changeHandler} value={formData.timeSlot} name='timeSlot' type='text' placeholder='Enter time(day/afternoon/evening)'></input>
+                <label>Time of Performance*</label>
+                <input onChange={changeHandler} value={formData.timeSlot} name='timeSlot' type='text' placeholder='Enter time(day/afternoon/evening/Night)'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Type of Artist</label>
-                <input onChange={changeHandler}  type='text' placeholder='Enter type of artist'></input>
+                <label>Position*</label>
+                <input onChange={changeHandler}  type='text' value={formData.position} placeholder='Enter Position' name='position'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Duration of Performance</label>
+                <label>Duration of Performance*</label>
                 <input onChange={changeHandler} value={formData.performanceDuration} name='performanceDuration' type='text' placeholder='Enter Performance Duration'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Artist Location</label>
-                <input onChange={changeHandler} value={formData.location} name='location' type='text' placeholder='Location of Artist'></input>
+                <label>Artist Level*</label>
+                <input onChange={changeHandler} value={formData.artistLevel} name='artistLevel' type='text' placeholder='Enter Artist Level'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Art Skills Required</label>
-                <input onChange={changeHandler}  type='text' placeholder='Enter multiple skills'></input>
+                <label>Expertise*</label>
+                <input onChange={changeHandler} value={formData.expertise} name='expertise' type='text' placeholder='Enter Artist Expertise'></input>
+              </div>
+              <div className='ArtistOpportunities_Page_Infoform_inputfield'>
+                <label>Category*</label>
+                <input onChange={changeHandler}  type='text' name='category' value={formData.category} placeholder='Enter Category'></input>
               </div>
             </div>
             <div className='ArtistOpportunities_Page_Infoform_Contenttwo'>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Proposed Budget</label>
+                <label>Proposed Budget*</label>
                 <input onChange={changeHandler} value={formData.budget} name='budget' type='text' placeholder='Enter proposed budget'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Last Date of Application</label>
-                <input onChange={changeHandler} value={formData.applicationPeriod.end} type='text' placeholder='Enter application last date'></input>
+                <label>Start Date of Application*</label>
+                <input onChange={changeHandler} name='applicationPeriod.start' value={formData.applicationPeriod.start} type='text' placeholder='Enter application Start date'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Any Other Requirements</label>
-                <input style={{height:"200px"}} type='text'></input>
+                <label>Last Date of Application*</label>
+                <input onChange={changeHandler} name='applicationPeriod.end' value={formData.applicationPeriod.end} type='text' placeholder='Enter application last date'></input>
               </div>
               <div className='ArtistOpportunities_Page_Infoform_inputfield'>
-                <label>Do you Offer any Additional Facilities</label>
-                <input style={{height:"200px"}} type='text'></input>
+                <label>Any Other Requirements*</label>
+                <input onChange={changeHandler} name='otherRequirements' value={formData.otherRequirements} style={{height:"200px"}} type='text' placeholder='Enter Any Other requirements' ></input>
+              </div>
+              <div className='ArtistOpportunities_Page_Infoform_inputfield'>
+                <label>Do you Offer any Additional Facilities(incentives)*</label>
+                <input onChange={changeHandler} name='incentives' value={formData.incentives} style={{height:"200px"}} type='text' placeholder='Enter Incentives'></input>
               </div>
             </div>
             <div className='ArtistOpportunities_Page_Infoform_btns'>
