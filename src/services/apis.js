4f77f01efaf24bfac3e508cef88ee28fd3985f28
abhishek kumar majumdar <pk006604@gmail.com>
2023-10-03 20:24:50 +0530

    dff

--- src/services/apis.js
+++ src/services/apis.js
@@ -37,7 +37,6 @@ export const patronProfilePoints = {
   FETCH_SINGLE__APPLI_API: BASE_URL + `/patrons/opportunities/applications`,
   GET_PATRON_APPLI_API: BASE_URL + `/patrons/opportunities`,
   UPDATE_OPPOR_API: BASE_URL + `/patrons/opportunities`,
-<<<<<<< HEAD
   GET_SINGLE_OPPOR_ALL_APPLIED_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=Applied&opportunityId=`,
   GET_SINGLE_OPPOR_ALL_IN_PROGRESS_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=In-Progress&opportunityId=`,
   GET_SINGLE_OPPOR_ALL_HIRED_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=Hired&opportunityId=`,
@@ -47,27 +46,21 @@ export const patronProfilePoints = {
   
 }
 
-=======
-  GET_SINGLE_OPPOR_ALL_APPLIED_ARTIST_APPLI_API:
-    BASE_URL +
-    `/patrons/opportunities/applications?status=Applied&opportunityId=`,
-  GET_SINGLE_OPPOR_ALL_IN_PROGRESS_ARTIST_APPLI_API:
-    BASE_URL +
-    `/patrons/opportunities/applications?status=In-Progress&opportunityId=`,
-  GET_SINGLE_OPPOR_ALL_HIRED_ARTIST_APPLI_API:
-    BASE_URL +
-    `/patrons/opportunities/applications?status=Hired&opportunityId=`,
+
+  GET_SINGLE_OPPOR_ALL_APPLIED_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=Applied&opportunityId=`,
+  GET_SINGLE_OPPOR_ALL_IN_PROGRESS_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=In-Progress&opportunityId=`,
+  GET_SINGLE_OPPOR_ALL_HIRED_ARTIST_APPLI_API: BASE_URL + `/patrons/opportunities/applications?status=Hired&opportunityId=`,
   GET_SINGLE_ARTIST_DATA_API: BASE_URL + `/patrons/artist-profiles?id=`,
-  HIRED_REJECT_SHORTLIST_ARTIST_API:
-    BASE_URL + `/patrons/opportunities/applications`,
+  HIRED_REJECT_SHORTLIST_ARTIST_API:BASE_URL + `/patrons/opportunities/applications`,
   PATRON_AVATAR_UPDATE: BASE_URL + "/patrons/profile/avatar",
-};
+;
+
+
 export const Admin = {
   GET_ALL_USERS: BASE_URL + `/admin/users`,
   GET_USER_BY_ID: BASE_URL + `/admin/users/`,
   GET_ARTIST_APPLICANTS: BASE_URL + `/admin/users/artist/`,
 };
->>>>>>> 47c20961800bcc17346959ed0c8098527749ed42
 
 export const contactUsPoints = {
   POST_QUERY_API: BASE_URL + `/quries/post-query`,
