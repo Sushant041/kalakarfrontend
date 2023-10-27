5e9a219f149f3b70a04caae43de1f39faa17aee0
Dibyendu Das <get2dibyendu@gmail.com>
2023-10-27 13:09:29 +0530

    opportunity card and details changed

--- src/My Components/ArtisitPages/ProfilePages/Artist_OpportunitiesMoreInfo.js
+++ src/My Components/ArtisitPages/ProfilePages/Artist_OpportunitiesMoreInfo.js
@@ -1,131 +1,147 @@
 import React, { useEffect } from "react";
-import {  useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "./Artist_OpportunitiesMoreInfo.css";
 import { useState } from "react";
 import Artist_navbar from "../Artist_navbar";
-import { toast } from 'react-toastify';
-  import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { useSelector } from "react-redux";
-import {  makeAuthenticatedPOSTRequest, makeAuthenticatedPOSTRequestWithoutBody } from "../../../services/serverHelper";
+import {
+  makeAuthenticatedPOSTRequest,
+  makeAuthenticatedPOSTRequestWithoutBody,
+} from "../../../services/serverHelper";
 import { artistOpportunityPoints } from "../../../services/apis";
 
-
 export function Artist_OpportunitiesMoreInfo() {
-
   const navigate = useNavigate();
 
   const location = useLocation();
 
   let job = location.state?.job;
 
-  console.log('job' , job);
+  console.log("job", job);
 
-  const [currentId , setCurrentId] = useState(null);
+  const [currentId, setCurrentId] = useState(null);
 
-  const [OpportunityapplynowPopup, setOpportunityapplynowPopup] = useState(null);
+  const [OpportunityapplynowPopup, setOpportunityapplynowPopup] =
+    useState(null);
 
   const { accessToken } = useSelector((state) => state.auth);
-  
+
   const [applyAns, setApplyAns] = useState("");
 
+  const [jobData, setJobData] = useState(null);
 
-  const [jobData , setJobData] = useState(null);
+  useEffect(() => {
+    if (job?.status === "Applied") {
+      setCurrentId(job?._id);
+      setJobData(job);
+    } else {
+      setCurrentId(job?._id);
+      setJobData(job);
+    }
+  }, []);
 
+  console.log("setjobda", jobData);
 
-
-  useEffect(()=>{
-   if(job?.status === 'Applied'){
-    setCurrentId(job?._id);
-    setJobData(job);
-   }
-   else {
-    setCurrentId(job?._id);
-    setJobData(job);
-   }
-  },[])
-
-
-  console.log('setjobda' , jobData);
-
-  console.log('curenid' ,currentId);
+  console.log("curenid", currentId);
 
   const applySubmitHandler = async (event) => {
-    const toastId = toast.loading("Loading..."  ,{
-      position:"top-center"
+    const toastId = toast.loading("Loading...", {
+      position: "top-center",
     });
     try {
       event.preventDefault();
-      const response = await makeAuthenticatedPOSTRequest(artistOpportunityPoints.APPLY_OPPOR_API + `/${job._id}`, { applyAns }, accessToken);
+      const response = await makeAuthenticatedPOSTRequest(
+        artistOpportunityPoints.APPLY_OPPOR_API + `/${job._id}`,
+        { applyAns },
+        accessToken
+      );
       console.log("response", response);
 
       if (response.status === "success") {
-        toast.success("successfully applied" , {
-          position:"top-center"
+        toast.success("successfully applied", {
+          position: "top-center",
         });
         setOpportunityapplynowPopup(null);
         setApplyAns("");
       } else {
-        toast.error(response.message , {
-          position:"top-center"
-        }) ;
+        toast.error(response.message, {
+          position: "top-center",
+        });
       }
     } catch (error) {
       console.log(error);
-      toast.error("server error , please try again" , {
-        position:"top-center"
+      toast.error("server error , please try again", {
+        position: "top-center",
       });
     }
 
     toast.dismiss(toastId);
   };
 
-const savedHandler = async()=>{
-  const toastId = toast.loading('Loading...');
-  try{
-
-    const response = await makeAuthenticatedPOSTRequestWithoutBody(artistOpportunityPoints.SAVE_OPPR_BY_ID + `/${currentId}`,accessToken);
-    console.log('response saved' , response);
-    if(response.status === 'success'){
-      toast.success('successfully Saved' , {
-        position:"top-center"
+  const savedHandler = async () => {
+    const toastId = toast.loading("Loading...");
+    try {
+      const response = await makeAuthenticatedPOSTRequestWithoutBody(
+        artistOpportunityPoints.SAVE_OPPR_BY_ID + `/${currentId}`,
+        accessToken
+      );
+      console.log("response saved", response);
+      if (response.status === "success") {
+        toast.success("successfully Saved", {
+          position: "top-center",
+        });
+        navigate("/statusOfApplication");
+      } else {
+        toast.error(response.message, {
+          position: "top-center",
+        });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("something went wrong , Please try again", {
+        position: "top-center",
       });
-           navigate("/statusOfApplication");
     }
-    else{
-      toast.error(response.message , {
-        position:"top-center"
-      });
-    }
+    toast.dismiss(toastId);
+  };
 
-  }catch(error){
-    console.log(error);
-    toast.error('something went wrong , Please try again' ,{
-      position:"top-center"
-    });
-  }
-  toast.dismiss(toastId);
-}
-
-useEffect(() => {
-  window.scrollTo(0, 0);
-}, []); 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
       <Artist_navbar />
       <div className="OpportunitiesMoreInfoPage">
         <div className="OpportunitiesMoreInfoPage_Topbox">
-          <h1 style={{color:"#AD2F3B"}}>{jobData?.position}</h1>
+          <h1 style={{ color: "#AD2F3B" }}>{jobData?.position}</h1>
           <div className="OpportunitiesMoreInfoPage_Topboxcontent">
             <div className="OpportunitiesPage_displayonejob_contentdetailsone">
               <p>
                 Category :&emsp;<span>{jobData?.category}</span>
               </p>
               <p>
-                Posted on :&emsp;<span>{new Date(jobData?.applicationPeriod?.start).toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })}</span>
+                Posted on :&emsp;
+                <span>
+                  {new Date(
+                    jobData?.applicationPeriod?.start
+                  ).toLocaleDateString("en-US", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </span>
               </p>
               <p>
-                Due Date :&emsp; <span>{new Date(jobData?.applicationPeriod?.end).toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })}</span>
+                Due Date :&emsp;{" "}
+                <span>
+                  {new Date(jobData?.applicationPeriod?.end).toLocaleDateString(
+                    "en-US",
+                    { day: "numeric", month: "short", year: "numeric" }
+                  )}
+                </span>
               </p>
               <p>
                 {" "}
@@ -134,7 +150,9 @@ useEffect(() => {
             </div>
             <div className="OpportunitiesMoreInfoPage_Topboxbtns">
               <button className="btnone">Share</button>
-              <button onClick={savedHandler} className="btntwo">Save</button>
+              <button onClick={savedHandler} className="btntwo">
+                Save
+              </button>
             </div>
           </div>
         </div>
@@ -145,64 +163,91 @@ useEffect(() => {
           <h1>Other Details</h1>
           <div className="OpportunitiesMoreInfoPage_bottombox_RSO">
             <div>
-              <p>Expertise :</p>
-              <p>Location : </p>
-              <p>Language : </p>
-              <p>Amount : </p>
-              <p>Theme : </p>
-              <p>Performance Time : </p>
+              <p>Type of Organisation/Patron :</p>
+              <p>Location of Performance : </p>
+              <p>Language of Performance : </p>
+              <p>Approx. Budget for Performance : </p>
+              <p>Date of Performance : </p>
               <p>Duration of Performance : </p>
-              <p>Nature of Art : </p>
-              <p>Performance Type : </p>
-              <p>Live / Recorded :</p>
-              <p>Level of Artist : </p>
-              <p>Artist Location :</p>
-              <p>Posted By : </p>
+              <p>Application Last Date : </p>
+              <p>Type of Art : </p>
+              <p>Category of Art :</p>
+              <p>Name of Art : </p>
+              <p>Theme of Performance : </p>
+              <p>Live/Recorded/Part Live :</p>
+              <p>Need customized/ curated Performance : </p>
+              <p>No. of required Artists : </p>
+              <p>Location of Artist : </p>
+              <p>Size of Audience : </p>
+              <p>Profile of Audience : </p>
+              <p>Venue of Performance : </p>
+              <p>Performance Facilities :</p>
+              <p>Any other Requirements : </p>
+              <p>Name of Contact Person : </p>
+              <p>Contact Number : </p>
+              <p>Email ID : </p>
             </div>
             <div>
-              <p>
-                <span>{jobData?.expertise} a</span>
-              </p>
+              <p>NA</p>
               <p>
                 <span>{jobData?.location} a</span>
               </p>
               <p>
                 <span>
                   {jobData?.languages?.map((lag, index) => (
-                    <span key={index}>{lag}  </span>
+                    <span key={index}>{lag} </span>
                   ))}
                 </span>
+                NA
               </p>
               <p>
                 <span>{jobData?.budget} </span>
               </p>
               <p>
-                <span>{jobData?.theme} </span>
-              </p>
-              <p>
-                <span>{jobData?.timeSlot} </span>
+                {new Date(jobData?.performanceDate).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
               </p>
               <p>
                 <span>{jobData?.performanceDuration} </span>
               </p>
               <p>
+                {new Date(jobData?.applicationPeriod.end).toLocaleDateString(
+                  "en-US",
+                  { day: "numeric", month: "short", year: "numeric" }
+                )}
+              
+              </p>
+              <p>
                 <span>{jobData?.artNature} </span>
+              </p>
+              <p>
+                <span>{jobData?.category} </span>
+              </p>
+              <p>NA</p>
+              <p>
+                <span>{jobData?.theme} </span>
               </p>
               <p>
                 <span>{jobData?.performanceType} </span>
               </p>
+              <p> NA </p>
               <p>
-                <span>{jobData?.mediaType} </span>
+                <span>{jobData?.requiredArtists} </span>
               </p>
+              <p> NA </p>
+              <p> NA </p>
+              <p> NA </p>
+              <p> NA </p>
+              <p> NA </p>
               <p>
-                <span>{jobData?.artLevel} </span>
+                <span>{jobData?.otherRequirements} </span>
               </p>
-              <p>
-                <span>{jobData?.location} </span>
-              </p>
-              <p>
-                <span>{jobData?.postedBy} </span>
-              </p>
+              <p> NA </p>
+              <p> NA </p>
+              <p> NA </p>
             </div>
           </div>
           <h1>Perks and Benefits</h1>
@@ -211,28 +256,26 @@ useEffect(() => {
               <p key={index}>{data}</p>
             ))}
           </div>
-          {
-            (job?.status === 'Applied' || job?.status === "In-Progress" || job?.status === "Rejected"  || job?.status === 'Hired') ?(
-              <div className="OpportunitiesMoreInfoPage_bottombox_btns">
-
-              <button className="notAppliedBtn" >
-                Already Applied
-              </button>
-              </div>
-            ):(
-              <div className="OpportunitiesMoreInfoPage_bottombox_btns">
+          {job?.status === "Applied" ||
+          job?.status === "In-Progress" ||
+          job?.status === "Rejected" ||
+          job?.status === "Hired" ? (
+            <div className="OpportunitiesMoreInfoPage_bottombox_btns">
+              <button className="notAppliedBtn">Already Applied</button>
+            </div>
+          ) : (
+            <div className="OpportunitiesMoreInfoPage_bottombox_btns">
               <button className="btnone" onClick={() => navigate(-1)}>
                 Back
               </button>
-              <button className="btntwo" onClick={() => setOpportunityapplynowPopup(true)}>
+              <button
+                className="btntwo"
+                onClick={() => setOpportunityapplynowPopup(true)}
+              >
                 Apply Now
               </button>
             </div>
-            )
-          }
-        
-
-          
+          )}
         </div>
 
         {/* this is for pop up  */}
@@ -242,15 +285,29 @@ useEffect(() => {
 
           <div className="Opportunityapplynowpopup_fullscreen">
             <div className="Opportunityapplynowpopup">
-              <button onClick={() => setOpportunityapplynowPopup(false)}>X</button>
+              <button onClick={() => setOpportunityapplynowPopup(false)}>
+                X
+              </button>
               <div className="Opportunityapplynowpopup_content">
                 <h4>{jobData?.title}</h4>
                 <div className="Opportunityapplynowpopup_contentone">
                   <p>
-                    Posted on: <span>{new Date(job.applicationPeriod.start).toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })}</span>
+                    Posted on:{" "}
+                    <span>
+                      {new Date(job.applicationPeriod.start).toLocaleDateString(
+                        "en-US",
+                        { day: "numeric", month: "short", year: "numeric" }
+                      )}
+                    </span>
                   </p>
                   <p>
-                    Last Date to apply: <span>{new Date(job.applicationPeriod.end).toLocaleDateString("en-US", { day: "numeric", month: "short", year: "numeric" })}</span>
+                    Last Date to apply:{" "}
+                    <span>
+                      {new Date(job.applicationPeriod.end).toLocaleDateString(
+                        "en-US",
+                        { day: "numeric", month: "short", year: "numeric" }
+                      )}
+                    </span>
                   </p>
                 </div>
               </div>
@@ -273,24 +330,34 @@ useEffect(() => {
                   Location :&emsp;&emsp; <span>{jobData?.location}</span>
                 </p>
                 <p>
-                  Language : &emsp; <span>{job.languages?.map((lag, index) => (
-                              <span key={index}>{lag} </span>
-                            ))}</span>
+                  Language : &emsp;{" "}
+                  <span>
+                    {job.languages?.map((lag, index) => (
+                      <span key={index}>{lag} </span>
+                    ))}
+                  </span>
                 </p>
                 <p>
                   Amount : &emsp; &emsp;<span>{jobData?.budget}</span>
                 </p>
                 <p>
                   {" "}
-                  Opening :&emsp;&emsp; <span>{jobData?.requiredArtists }</span>
+                  Opening :&emsp;&emsp; <span>{jobData?.requiredArtists}</span>
                 </p>
               </div>
               <h1>Why do you want ot Apply for this Role?</h1>
               <div className="Opportunityapplynowpopup_contentform">
                 <form onSubmit={applySubmitHandler}>
-                  <textarea name="applyAns" value={applyAns} onChange={(e) => setApplyAns(e.target.value)} required />
+                  <textarea
+                    name="applyAns"
+                    value={applyAns}
+                    onChange={(e) => setApplyAns(e.target.value)}
+                    required
+                  />
                   <button type="submit">Submit</button>
-                  <button onClick={() => setOpportunityapplynowPopup(false)}>Cancel</button>
+                  <button onClick={() => setOpportunityapplynowPopup(false)}>
+                    Cancel
+                  </button>
                 </form>
               </div>
             </div>
